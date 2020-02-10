b7e147b2dfa965de5e8fd3e1c9249d7dbd9bba99
r105 <ronaldbeltre05@gmail.com>
2020-02-10 01:07:21 -0400

    Adding all

---
+++ WebAPI_Ex_4/Controllers/MoviesController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI_Ex_4.Models.Entities;
+using WebAPI_Ex_4.Services;
+
+namespace WebAPI_Ex_4.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        private Ex_4Entities db = new Ex_4Entities();
+        private readonly MovieService _movieService = new MovieService();
+
+
+        [HttpGet]
+        [Route("api/movies")]
+        public List<Movie> GetMovies()
+        {
+            return _movieService.GetMyMovies();
+        }
+
+        [HttpGet]
+        [Route("api/movies/{id}")]
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult GetMovie([FromUri] int id)
+        {
+            var movie = _movieService.GetMyMovie(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        [HttpPut]
+        [Route("api/updateMovie/{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMovie([FromUri] int id, [FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _movieService.UpdateMovie(id, movie);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        
+        [HttpPost]
+        [Route("api/createMovie")]
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult PostMovie([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _movieService.CreateMovie(movie);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        
+        [HttpDelete]
+        [Route("api/deleteMovie/{id}")]
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult DeleteMovie([FromUri] int id)
+        {
+            
+            if (db.Movies.Find(id)  == null)
+            {
+                return NotFound();
+            }
+
+            _movieService.RemoveMovie(id);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+    }
+}
---
+++ WebAPI_Ex_4/Services/MovieService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using WebAPI_Ex_4.Models.Entities;
+
+namespace WebAPI_Ex_4.Services
+{
+    public class MovieService
+    {
+        private readonly Ex_4Entities _dbContext = new Ex_4Entities();
+
+        public List<Movie> GetMyMovies()
+        {
+            return _dbContext.Movies.ToList();
+        }
+
+        public Movie GetMyMovie(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+
+            return movie;
+        }
+
+        public void UpdateMovie(int id, Movie movie)
+        {
+            var movieToUpdate = _dbContext.Movies.Find(id);
+
+            if (movieToUpdate != null)
+            {
+                movieToUpdate.Id = movie.Id;
+                movieToUpdate.Name = movie.Name;
+                movieToUpdate.Date = movie.Date;
+                movieToUpdate.Owner = movie.Owner;
+            }
+
+            _dbContext.SaveChanges();
+
+        }
+
+        public void CreateMovie(Movie movie)
+        {
+
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
+
+        }
+
+        public void RemoveMovie(int id)
+        {
+
+            var movie = _dbContext.Movies.Find(id);
+
+            _dbContext.Movies.Remove(movie ?? throw new InvalidOperationException());
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
